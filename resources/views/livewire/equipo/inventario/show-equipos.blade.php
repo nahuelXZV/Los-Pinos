f4a9bfa2fa8fd8938a1c96385c45be5818d05601
NahuelZ <zalazanahuel43@gmail.com>
2021-12-05 16:28:51 -0400

    .

--- resources/views/livewire/equipo/inventario/show-equipos.blade.php
+++ resources/views/livewire/equipo/inventario/show-equipos.blade.php
@@ -301,11 +301,11 @@
 
                             </td>
 
-                            <td class="px-6 py-4 text-sm text-white font-bold">
+                            <td class="px-6 py-4 text-sm text-gray-500 font-bold">
                                 @if ($equipo->stock == null)
-                                <span class="px-2 rounded-full inline-flex bg-gray-500">
-                                    Vacio
-                                </span>
+                                    <span class="px-2 rounded-full inline-flex text-white bg-gray-500">
+                                        Vacio
+                                    </span>
                                 @else
                                     {{ $equipo->stock }}
                                 @endif
@@ -355,17 +355,17 @@
                                 <td class=" my-3 inline-flex justify-center px-6 py-4 whitespace-nowrap flex">
                                     @can('equipos.edit')
 
-                                    <div class="whitespace-nowrap flex">
-                                        <a class="font-bold text-white rounded cursor-pointer bg-blue-600 hover:bg-blue-500 py-2 px-4"
-                                            wire:click='open({{ $equipo->codigo }})'>
-                                
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                                                stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                            </svg>
-                                        </a>
-                                    </div>
+                                        <div class="whitespace-nowrap flex">
+                                            <a class="font-bold text-white rounded cursor-pointer bg-blue-600 hover:bg-blue-500 py-2 px-4"
+                                                wire:click='open({{ $equipo->codigo }})'>
+
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                                                    viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                                </svg>
+                                            </a>
+                                        </div>
 
                                     @endcan
 
